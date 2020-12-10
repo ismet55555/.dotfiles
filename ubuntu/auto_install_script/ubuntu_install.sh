233a3baf965fe32eb8987ab95dec1674859bd165
Ismet Handzic <ismet55555@users.noreply.github.com>
2020-12-10 12:33:07 -0500

    Update ubuntu_install.sh

--- ubuntu/auto_install_script/ubuntu_install.sh
+++ ubuntu/auto_install_script/ubuntu_install.sh
@@ -199,6 +199,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 #           plugins=(zsh-syntax-highlighting)
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
+# Changing user shell to ZSH
+chsh -s $(which zsh)
 
 
 ###############################################################################
