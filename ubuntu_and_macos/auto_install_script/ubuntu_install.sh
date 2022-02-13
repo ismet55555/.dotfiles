83735d82854ad3aa6e1831a627d87f0699887c31
Ismet Handzic <ismet55555@users.noreply.github.com>
2022-02-12 21:43:31 -0500

    Update ubuntu_install.sh

--- ubuntu_and_macos/auto_install_script/ubuntu_install.sh
+++ ubuntu_and_macos/auto_install_script/ubuntu_install.sh
@@ -282,6 +282,15 @@ wget https://github.com/sharkdp/bat/releases/download/v0.17.1/bat_0.17.1_amd64.d
 dpkg -i bat_0.17.1_amd64.deb
 
 
+# exa (instead of ls)
+echo
+echo "============== Installing: bat =================="
+EXA_VERSION=$(curl -s "https://api.github.com/repos/ogham/exa/releases/latest" | grep -Po '"tag_name": "v\K[0-9.]+')
+curl -Lo exa.zip "https://github.com/ogham/exa/releases/latest/download/exa-linux-x86_64-v${EXA_VERSION}.zip"
+sudo unzip -q exa.zip bin/exa -d /usr/local
+rm -rf exa.zip
+
+
 
 ###############################################################################
 #                               Fun Stuff
